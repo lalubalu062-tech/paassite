b29bb383920bbf7b782261a8c08afeb38b01ae6b
Lalu Kumar <lalubalu062@gmail.com>
2026-07-12 20:33:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Yeh code Render ke Logs mein print karega ki server ke andar exactly kya-kya rakha hai
-    print("=== DEBUGGING INFO ===")
-    print("Current Folder:", os.getcwd())
-    print("Files in Main Folder:", os.listdir('.'))
-    if os.path.exists('templates'):
-        print("Files inside 'templates':", os.listdir('templates'))
-    else:
-        print("WARNING: 'templates' folder is MISSING in Render!")
-    print("======================")
-    
     return render_template('index.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        # Yahan aage chalkar database logic aayega
         return redirect(url_for('login'))
     return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        # Yahan aage chalkar verification aayega
         return redirect(url_for('dashboard'))
     return render_template('login.html')
 
