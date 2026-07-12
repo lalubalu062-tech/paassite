b0ec7b301f566d1978ac254827b35c78d9df4ac8
Lalu Kumar <lalubalu062@gmail.com>
2026-07-12 19:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Yeh aapke templates folder se index.html ko load karega
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
